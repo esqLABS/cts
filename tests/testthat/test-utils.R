81b75c02ac8b823d32d44932ffd9eba9825cc21a
Felix MIL <felix.mil@esqlabs.com>
2024-09-16 09:36:41 +0200

    add tests for utils

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,15 @@
+test_that("check_internet works",{
+  expect_no_error(check_internet())
+})
+
+test_that("is_file_url works",{
+  expect_true(is_file_url("https://raw.githubusercontent.com/Open-Systems-Pharmacology/OSPSuite.BuildingBlockTemplates/main/templates.json"))
+  expect_false(is_file_url("path/to/file"))
+})
+
+test_that("is_file_local works",{
+
+  expect_true(is_file_local(test_path("data", "Itraconazole-Model.json")))
+  expect_false(is_file_local(test_path("data")))
+
+})
