8f1ea2ad2ad81376991fa77f306094ea4f8250d1
Felix MIL <felix.mil@esqlabs.com>
2024-09-19 15:30:17 +0200

    limit namespace import to minimum

---
+++ R/cts-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @import rlang
+#' @import cli
+
+## usethis namespace: start
+#' @importFrom purrr keep
+#' @importFrom purrr list_c
+#' @importFrom purrr list_flatten
+#' @importFrom purrr map
+#' @importFrom purrr map_int
+#' @importFrom purrr map_vec
+#' @importFrom purrr walk
+## usethis namespace: end
+NULL
--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,1 @@
-#' @import rlang
-#' @import purrr
-#' @import cli
-#' @import jsonlite
-#' @import ospsuite
-#' @import R6
-#' @import snakecase
-NULL
-
 model_library_url <- "https://raw.githubusercontent.com/Open-Systems-Pharmacology/OSPSuite.BuildingBlockTemplates/main/templates.json"
