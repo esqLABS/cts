71dcde3b9ff06e996a0a17bff67db65a7ce650ae
Diane Lefaudeux <151058642+dlefaudeux@users.noreply.github.com>
2025-02-14 19:03:43 +0100

    add tests

---
+++ tests/testthat/test-formulation.R
@@ -0,0 +1,35 @@
+test_that("Formulation can be initialized", {
+  expect_no_message(Formulation$new(name = "Test formulation", type = "dissolved"))
+})
+
+test_that("Dissolved formulation can be created", {
+  expect_snapshot(create_formulation(name = "Solution", type = "dissolved"))
+})
+
+test_that("Weibull formulation can be created", {
+  expect_snapshot(create_formulation(name = "Weibull", type = "weibull"))
+})
+
+test_that("Lint80 formulation can be created", {
+  expect_snapshot(create_formulation(name = "Lint", type = "lint80"))
+})
+
+test_that("Particle monodisperse formulation can be created", {
+  expect_snapshot(create_formulation(name = "ParticleMono", type = "particle"))
+})
+
+test_that("Particle polydisperse normal formulation can be created", {
+  expect_snapshot(create_formulation(name = "ParticlePolyNormal", type = "particle", distribution_type = "poly"))
+})
+
+test_that("Particle polydisperse lognormal formulation can be created", {
+  expect_snapshot(create_formulation(name = "ParticlePolyLogNormal", type = "particle", distribution_type = "poly", particle_size_distribution = "lognormal"))
+})
+
+test_that("Zero order formulation can be created", {
+  expect_snapshot(create_formulation(name = "ZeroOrder", type = "zero"))
+})
+
+test_that("First order formulation can be created", {
+  expect_snapshot(create_formulation(name = "FirstOrder", type = "first"))
+})
