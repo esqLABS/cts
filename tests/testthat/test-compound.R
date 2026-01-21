5aff41d351c60c59e98dcbe54db5e3d332db6f03
Diane Lefaudeux <151058642+dlefaudeux@users.noreply.github.com>
2026-01-21 09:06:50 +0100

    Update tests

--- tests/testthat/test-compound.R
+++ tests/testthat/test-compound.R
@@ -4,4 +4,17 @@ test_that("Compound can be initialized", {
       compound("Rifampicin")
     })
   )
+
+
+  expect_warning(
+    suppressMessages({
+      compound("Digoxin")
+    }), "UserDefined administration is not allowed"
+  )
+
+  expect_warning(
+    suppressMessages({
+      compound("Coproporphyrin I")
+    }), "UserDefined administration is not allowed"
+  )
 })
